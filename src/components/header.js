700eb79d994083d50c1dca5161952e2507e29945
Justin Stonecipher <justin.stonecipher@cen.centeva.com>
2019-07-01 15:08:19 -0600

    update header and add about

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,9 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import Image from "../components/image"
 
-const Header = ({ siteTitle }) => (
+const Header = ({ home, about, solutions, contractVehicles, contact }) => (
   <header
     style={{
       background: `rebeccapurple`,
@@ -12,11 +13,21 @@ const Header = ({ siteTitle }) => (
     <div
       style={{
         margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        display: `flex`,
+        justifyContent: `space-around`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
+      <div
+        style={{
+          width: `300px`,
+          marginBottom: `1.45rem`,
+        }}
+      >
+        <Link to="/">
+          <Image />
+        </Link>
+      </div>
+      <h4 style={{ margin: 0, paddingTop: `30px` }}>
         <Link
           to="/"
           style={{
@@ -24,19 +35,73 @@ const Header = ({ siteTitle }) => (
             textDecoration: `none`,
           }}
         >
-          {siteTitle}
+          {home}
         </Link>
-      </h1>
+      </h4>
+      <h4 style={{ margin: 0, paddingTop: `30px` }}>
+        <Link
+          to="/about/"
+          style={{
+            color: `white`,
+            textDecoration: `none`,
+          }}
+        >
+          {about}
+        </Link>
+      </h4>
+      <h4 style={{ margin: 0, paddingTop: `30px` }}>
+        <Link
+          to="/"
+          style={{
+            color: `white`,
+            textDecoration: `none`,
+          }}
+        >
+          {solutions}
+        </Link>
+      </h4>
+      <h4 style={{ margin: 0, paddingTop: `30px` }}>
+        <Link
+          to="/"
+          style={{
+            color: `white`,
+            textDecoration: `none`,
+          }}
+        >
+          {contractVehicles}
+        </Link>
+      </h4>
+      <h4 style={{ margin: 0, paddingTop: `30px` }}>
+        <Link
+          to="/"
+          style={{
+            color: `white`,
+            textDecoration: `none`,
+          }}
+        >
+          {contact}
+        </Link>
+      </h4>
     </div>
   </header>
 )
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
+  home: PropTypes.string,
+  about: PropTypes.string,
+  solutions: PropTypes.string,
+  contractVehicles: PropTypes.string,
+  contact: PropTypes.string,
 }
 
 Header.defaultProps = {
   siteTitle: ``,
+  home: `Home`,
+  about: `About Us`,
+  solutions: `Solutions`,
+  contractVehicles: `Contract Vehicles`,
+  contact: `Contact Us`,
 }
 
 export default Header
---
+++ src/pages/about.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Layout from "../components/layout"
+
+export default () => (
+    <Layout>
+        <p>Such wow. Very React...(I dunno the tutorial told me to do it)</p>
+    </Layout>
+)
